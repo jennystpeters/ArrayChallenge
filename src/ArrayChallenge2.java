d24cc74aec0a420a461da1d9e7cc5fc3c58b22d7
jennystpeters <jennystpeters@gmail.com>
2017-07-06 10:29:36 -0400

    Array Challenge First Commit

---
+++ src/ArrayChallenge2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * Created by jenny on 7/6/2017.
+ */
+public class ArrayChallenge2 {
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+
+        int sum = 0;
+
+        System.out.println("Please enter number of rows: ");
+        System.out.println("Please enter number");
+
+        int[][] array = new int[3][3];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                System.out.printf("Please enter an integer for row %d column %d:    ", i, j);
+                array[i][j] = entry.nextInt();
+
+                sum += array[i][j];
+
+            }
+        }
+        System.out.println();
+
+        sum = sum - (array[1][0] + array[1][2]);
+        System.out.println("Hourglass sum is " + sum);
+    }
+}
---
+++ src/ArrayInputChallenge.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by jenny on 7/6/2017.
+ */
+public class ArrayInputChallenge {
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+
+        int sum = 0;
+
+        int[][] array = new int[3][3];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                System.out.printf("Please enter an integer for row %d column %d:    ", i, j);
+                array[i][j] = entry.nextInt();
+
+                sum += array[i][j];
+
+            }
+        }
+        System.out.println();
+
+        sum = sum - (array[1][0] + array[1][2]);
+        System.out.println("Hourglass sum is " + sum);
+
+   }
+}
